eec8a086dd077b82f584e4d97742b7c58eabe844
ylanbenoliel <ylanbenoliel@gmail.com>
2019-11-11 20:46:15 -0300

    Create SignIn screen

--- src/pages/signIn/index.js
+++ src/pages/signIn/index.js
@@ -1,12 +1,67 @@
-import React from 'react';
-import { View, Text} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StatusBar, AsyncStorage } from 'react-native';
+import { StackActions, NavigationAction } from 'react-navigation'
 
-// import { Container } from './styles';
+import api from '../../services/api'
 
-export default function signIn() {
+import {
+  Container,
+  Logo,
+  Input,
+  ErrorMessage,
+  Button,
+  ButtonText,
+  SignUpLink,
+  SignUpLinkText
+} from './styles';
+
+export default function signIn(props) {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+
+  function handleEmailChange(email) {
+    setEmail(email)
+  }
+  function handlePasswordChange(password) {
+    setPassword(password)
+  }
+
+  function handleSignInPress() {
+
+  }
+
+  function handleCreateAccountPress() {
+
+  }
+
   return (
-    <View>
-      <Text>signIn</Text>
-    </View>
-  );
+    <Container>
+      <StatusBar hidden />
+      <Logo
+        source={require('../../images/airbnb_logo.png')} resizeMode="contain" />
+      <Input
+        placeholder="Endereço de e-mail"
+        value={email}
+        onChangeText={handleEmailChange}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <Input
+        placeholder="Senha"
+        value={password}
+        onChangeText={handlePasswordChange}
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry
+      />
+      {error.length !== 0 && <ErrorMessage>{error}</ErrorMessage>}
+      <Button onPress={() => handleSignInPress}>
+        <ButtonText>Entrar</ButtonText>
+      </Button>
+      <SignUpLink onPress={() => handleCreateAccountPress}>
+        <SignUpLinkText>Criar conta grátis</SignUpLinkText>
+      </SignUpLink>
+    </Container>
+  )
 }
