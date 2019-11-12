66135409dfde0382d65d3bc52f289028eb4dc1ec
ylanbenoliel <ylanbenoliel@gmail.com>
2019-11-12 19:34:51 -0300

    Show map feature

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StatusBar } from 'react-native';
 import MapBoxGL from '@react-native-mapbox-gl/maps'
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function main() {
   const coords = [[-49.6446024, -27.2108001]]
   return (
-    <MapBoxGL.MapView
-      ref={c => (_map = c)}
-      style={{ flex: 1 }}
-      styleURL={MapBoxGL.StyleURL.Dark}
-    >
-      <MapBoxGL.Camera
-        zoomLevel={9}
-        centerCoordinate={coords[0]}
-      />
-    </MapBoxGL.MapView>
+    <Container>
+      <StatusBar barStyle='light-content' />
+      <MapBoxGL.MapView
+        ref={c => (_map = c)}
+        style={{ flex: 1 }}
+        styleURL={MapBoxGL.StyleURL.Dark}
+      >
+        <MapBoxGL.Camera
+          zoomLevel={16}
+          centerCoordinate={coords[0]}
+        />
+      </MapBoxGL.MapView>
+    </Container>
   );
 }
